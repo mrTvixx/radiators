142fb01b73382988388669143f7fb2a93e9aeac6
Valeriy Kuznetsov <dev.valeriy@gmail.com>
2021-01-17 11:28:47 +0300

    fix tooltips

--- components/Header/InfoBlock/index.vue
+++ components/Header/InfoBlock/index.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="company-block">
-    <router-link to="/" class="company-block__logo">
-      <span>Дом</span>
-      <span class="red">В</span>
-      <span>Тепле</span>
-    </router-link>
+    <div class="company-block__logo-container">
+      <div
+        v-if="width <= 1040"
+        v-tooltip="{
+          content: 'Нужна помощь? <br /> Закажи звонок.',
+          show: isTooltip,
+          trigger: 'none',
+        }"
+        class="company-block__get-call--mobile"
+        @click="call"
+      >
+        <v-icon class="company-block__svg" name="phone-call"></v-icon>
+        {{ " Заказать звонок" }}
+      </div>
+      <router-link to="/" class="company-block__logo">
+        <span>Дом</span>
+        <span class="red">В</span>
+        <span>Тепле</span>
+      </router-link>
+    </div>
     <nav class="company-block__menu">
       <router-link
         v-for="item in linksList"
@@ -15,8 +30,9 @@
       >
     </nav>
     <div
+      v-if="width > 1040"
       v-tooltip="{
-        content: 'Нужны помощь? <br /> Закажи звонок.',
+        content: 'Нужна помощь? <br /> Закажи звонок.',
         show: isTooltip,
         trigger: 'none',
       }"
@@ -24,11 +40,12 @@
       @click="call"
     >
       <v-icon class="company-block__svg" name="phone-call"></v-icon>
-      <span name="callText" class="company-block__get-call-text">Заказать звонок </span>
+      {{ "Заказать звонок" }}
     </div>
     <CallModal :withoutAutoOpen="true" ref="callModal" />
     <span class="company-block__number">
-      <a href="tel:+74994443465" class="phone">+7 499 444 34 65</a>
+      <a href="tel:+74994443465" class="contact">+7 499 444 34 65</a>
+      <a href="mailto:info@домвтепле.рф">info@домвтепле.рф</a>
       <span class="info">
         Ежедневно с 9
         <sup>00</sup>-20
@@ -50,9 +67,13 @@ export default {
     return {
       linksList: links,
       isTooltip: false,
+      width: 0,
     };
   },
   mounted() {
+    window.addEventListener("resize", this.updateWidth);
+    this.updateWidth();
+
     const isCall = localStorage.getItem("isCall");
     const currentDate = new Date().getDate();
     if (String(isCall) === String(currentDate)) return;
@@ -63,6 +84,9 @@ export default {
     }, 5000);
   },
   methods: {
+    updateWidth() {
+      this.width = window.innerWidth;
+    },
     call() {
       this.isTooltip = false;
       this.$refs.callModal.$refs.call.open();
@@ -83,11 +107,18 @@ export default {
     font-size: 20px;
     cursor: pointer;
     width: 350px;
-    padding: 15px 5px 0;
+    padding: 15px 5px 4px;
+    display: flex;
+    align-items: flex-end;
+
+    &--mobile {
+      display: none;
+    }
   }
 
   &__svg {
     width: 18px;
+    padding: 0 10px 0 0;
   }
 
   .red {
@@ -112,6 +143,13 @@ export default {
   &__logo {
     cursor: pointer;
     min-width: 210px;
+    align-items: flex-end;
+    display: flex;
+    justify-content: space-around;
+
+    &-container {
+      display: flex;
+    }
 
     span {
       font-size: 2.3rem;
@@ -123,7 +161,7 @@ export default {
     max-width: 1000px;
     width: 100%;
     display: flex;
-    padding: 0 0 6px 30px;
+    padding: 0 0 4px 30px;
     align-items: flex-end;
 
     &-item {
@@ -138,15 +176,43 @@ export default {
   }
 }
 
-.phone {
+.contact {
   color: $project-red;
-  font-size: 1.24rem;
 }
 
 @media (max-width: 1040px) {
   .company-block {
     &__menu {
       display: none;
+    }
+    &__logo {
+      min-width: 165px;
+      display: flex;
+
+      &-container {
+        flex-flow: column;
+      }
+
+      span {
+        font-size: 1.8rem;
+      }
+    }
+
+    &__get-call {
+      width: unset;
+      padding: 10px 0 0 20px;
+      display: none;
+      width: 190px;
+
+      &--mobile {
+        width: 100%;
+        font-size: 16px !important;
+        display: block;
+      }
+    }
+
+    &__svg {
+      padding: 0;
     }
   }
 }
@@ -157,13 +223,33 @@ export default {
       min-width: 125px;
     }
 
+    &__logo {
+      min-width: 165px;
+      display: flex;
+
+      &-container {
+        flex-flow: column;
+      }
+
+      span {
+        font-size: 1.8rem;
+      }
+    }
+
     &__get-call {
       width: unset;
       padding: 10px 0 0 20px;
+      display: none;
 
-      &-text {
-        display: none;
+      &--mobile {
+        width: 100%;
+        font-size: 16px !important;
+        display: block;
       }
+    }
+
+    &__svg {
+      padding: 0;
     }
 
     .info {
@@ -172,14 +258,6 @@ export default {
 
     .phone {
       font-size: 1rem;
-    }
-
-    &__logo {
-      min-width: 165px;
-
-      span {
-        font-size: 1.8rem;
-      }
     }
 
     .red {
